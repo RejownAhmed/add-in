af842e11d8e5aebbd8cf5e82956df175147408a3
“rejownahmed” <rejownahmedzisan@gmail.com>
2023-10-15 00:23:39 +0600

    Js file updated

--- src/taskpane/js/assignsignature.js
+++ src/taskpane/js/assignsignature.js
@@ -1,4 +1,18 @@
-function renderMainPage() {
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    // showLoader(), initConstants(), void 0 === _isFirstTime || _isFirstTime ? showWelcomeSection() :
+    renderMainPage();
+  }
+});
+
+async function renderMainPage() {
   Office.context.mailbox.item.saveAsync(function (e) {
     console.log(e, "assignsignature page");
   });
