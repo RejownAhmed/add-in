5c9f4377422a9742e28d87beb965f1043e0c1f39
Usman1104 <usmaantic@gmail.com>
2024-01-08 15:22:11 +0500

    Signature

--- src/prod/commands/js/autorun.js
+++ src/prod/commands/js/autorun.js
@@ -1,77 +1,162 @@
 const SIGNATURE_API_URL = "api/v2/app/reach/get-signature";
 const API_BASE_URL = getBaseUrl('production');
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
-// let isFirstTime = 0,
-//   signature;
+// Contains code for event-based activation on Outlook on web, on Windows, and on Mac (new UI preview).
 
-// function init() {
-//   signature = Office.context.roamingSettings.get("signature");
-//   isFirstTime = Office.context.roamingSettings.get("isFirstTime");
+/**
+ * Checks if signature exists.
+ * If not, displays the information bar for user.
+ * If exists, insert signature into new item (appointment or message).
+ * @param {*} eventObj Office event object
+ * @returns
+ */
+function checkSignature(eventObj) {
+   
 
-//   console.log(isFirstTime);
-// }
+  Office.context.mailbox.item.from.getAsync(function (asyncResult) {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+          const msgFrom = asyncResult.value;
+          console.log("Message from: " + msgFrom.emailAddress);
+          insert_auto_signature(msgFrom.emailAddress, eventObj);
+      } else {
+          console.log(asyncResult.error);
+      }
+  });
 
-function getSignature(token) {
-  // `${_host}/domain/user/view_signature/raw?token=${e}&email=${_email}&client=outlook&platform=${_platform}&client_version=${_client_version}&addin_js_version=1.3.0`
-  const email = Office.context.mailbox.userProfile.emailAddress;
-  const url = `${API_BASE_URL}/${SIGNATURE_API_URL}/outlook/`;
-  return axios.get(url, {
-    params: {
-      email,
-      token,
-    },
+ //// if (eventObj.source.id == "OnNewMessageCompose") {
+ //     let user_info_str = Office.context.roamingSettings.get("user_info");
+ //     let user_info = JSON.parse(user_info_str);
+ //   let  user_email = user_info.email;
+
+ // insert_auto_signature(user_email, eventObj);
+
+}
+
+  function insert_auto_signature(user_info, eventObj)
+  {
+      if (Office.context.mailbox.item.getComposeTypeAsync) {
+          // Find out if the compose type is "newEmail", "reply", or "forward" so that we can apply the correct template.
+          Office.context.mailbox.item.getComposeTypeAsync(
+              {
+                  asyncContext: {
+                      user_info: user_info,
+                      eventObj: eventObj,
+                  },
+              },
+              function (asyncResult) {
+                  if (asyncResult.status === "succeeded") {
+                      get_signature_info(
+                          asyncResult.asyncContext.user_info,
+                          asyncResult.asyncContext.eventObj
+                          
+                      );
+                  }
+              }
+          );
+      }
+
+
+  
+}
+
+
+function addTemplateSignature(signatureDetails, eventObj) {
+
+  //Image is not embedded, or is referenced from template HTML
+  Office.context.mailbox.item.body.setSignatureAsync(
+      signatureDetails,
+      {
+          coercionType: "html",
+          asyncContext: eventObj,
+      },
+      function (asyncResult) {
+          asyncResult.asyncContext.completed();
+      }
+  );
+
+}
+
+/**
+* Creates information bar to display when new message or appointment is created
+*/
+function display_insight_infobar() {
+  Office.context.mailbox.item.notificationMessages.addAsync("fd90eb33431b46f58a68720c36154b4a", {
+      type: "insightMessage",
+      message: "Please set your signature with the Office Add-ins sample.",
+      icon: "Icon.16x16",
+      actions: [
+          {
+              actionType: "showTaskPane",
+              actionText: "Set signatures",
+              commandId: get_command_id(),
+              contextData: "{''}",
+          },
+      ],
   });
 }
 
-function checkSignature(e) {
-  // init();
-  // checkIfFirstTime();
-  // console.log(e);
-  Office.context.mailbox.item.saveAsync(function (t) {
-    // console.log(t);
-    if (t.status === Office.AsyncResultStatus.Succeeded) {
-      Office.context.mailbox.getCallbackTokenAsync({ isRest: !0 }, function (t) {
-        getSignature(t.value)
-          .then((res) => {
-            Office.context.mailbox.item.body.setSignatureAsync(res.data, {
-              coercionType: Office.MailboxEnums.BodyType.Html,
-            });
-            // TODO: Later
-            // saveUserSignature(res.data);
+/**
+* Gets template name (A,B,C) mapped based on the compose type
+* @param {*} compose_type The compose type (reply, forward, newMail)
+* @returns Name of the template to use for the compose type
+*/
+function get_template_name(compose_type) {
+  if (compose_type === "reply") return Office.context.roamingSettings.get("reply");
+  if (compose_type === "forward") return Office.context.roamingSettings.get("forward");
+  return Office.context.roamingSettings.get("newMail");
+}
+
+/**
+* Gets HTML signature in requested template format for given user
+* @param {\} template_name Which template format to use (A,B,C)
+* @param {*} user_info Information details about the user
+* @returns HTML signature in requested template format
+*/
+function get_signature_info(user_info, eventObj) {
+  let apiUrl = "https://reachapi.reach.app/api/v2/app/reach/get-signature/outlook?email=" + user_info;
+
+      fetch(apiUrl)
+          .then(function (response) {
+              if (!response.ok) {
+                  console.log("Network response was not ok");
+              }
+              return response.text();
           })
-          .catch((error) => {
-            if (error.response?.data?.message) {
-              console.error(error.response?.data?.message);
-            } else {
-              console.log(error);
-            }
+          .then(function (signature) {
+              // Assuming the API returns the signature as text
+              // You may need to adjust this part based on the actual response format
+            
+              console.log(signature);
+              if (signature !== "") {
+                  addTemplateSignature(signature,eventObj)
+
+                  // Your logic when the signature is not empty
+              }
+          })
+          .catch(function (error) {
+              console.log('Fetch error:', error);
+              // Handle errors as needed
           });
-      });
-    } else {
-      console.error("Item save failed: " + t.error.message);
-    }
-  });
-  e.completed();
+
+
+  
+
 }
 
-// TODO: Later implement below methods
 
-// function checkIfFirstTime() {
-//   saveRoamingSettings("isFirstTime", void 0 === isFirstTime ? !signature : isFirstTime);
-// }
 
-// function saveUserSignature(signature) {
-//   Office.context.roamingSettings.set("signature", signature);
-//   Office.context.roamingSettings.saveAsync(function (e) {
-//     console.log("saveToRoamingSettings - " + JSON.stringify(e));
-//   });
-// }
+/**
+* Gets correct command id to match to item type (appointment or message)
+* @returns The command id
+*/
+function get_command_id() {
+  if (Office.context.mailbox.item.itemType == "appointment") {
+      return "MRCS_TpBtn1";
+  }
+  return "MRCS_TpBtn0";
+}
 
-// function saveRoamingSettings(key, payload) {
-//   Office.context.roamingSettings.set(key, payload);
-//   Office.context.roamingSettings.saveAsync(function (e) {
-//     console.log("saveToRoamingSettings - " + JSON.stringify(e));
-//   });
-// }
 
 Office.actions.associate("checkSignature", checkSignature);
